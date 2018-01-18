bbc79016b11884b6033e06aa1389221d873e76f0
kat528 <kat528@users.noreply.github.com>
2018-01-18 18:29:02 +0200

    Speech

--- app/src/main/java/com/holomedia/holomedia/MainActivity.java
+++ app/src/main/java/com/holomedia/holomedia/MainActivity.java
@@ -31,6 +31,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Locale;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -142,11 +143,11 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH);
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
                 "Hi speak something");
         try {
-            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+            startActivityForResult(intent, 200);
         } catch (ActivityNotFoundException a) {
             Toast.makeText(getApplicationContext(),
                     "Sorry! Your device doesn\\'t support speech input",
@@ -161,7 +162,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
             if(requestCode == 200) {
-                if (resultCode == RESULT_OK && null != data) {
+                if (resultCode == RESULT_OK && data != null) {
 
                     ArrayList<String> result = data
                             .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
@@ -172,15 +173,13 @@ public class MainActivity extends AppCompatActivity {
                     String file = readFromFile();
                     String titles[] = file.split("\n");
                     boolean found = false;
-                    String title = null;
-                    for(int i =0; i< titles.length; i++){
-                        if (speechResult == titles[i]){
+                    for (String title1 : titles) {
+                        if (Objects.equals(speechResult, title1)) {
                             found = true;
-                            title = titles[i];
                         }
                     }
                     if (found) {
-                        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + getResources().getIdentifier(title, "drawable", getPackageName()));
+                        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + getResources().getIdentifier(speechResult, "raw", getPackageName()));
                         Intent a = new Intent(MainActivity.this, PlayVideo.class);
                         a.putExtra("uri", uri);
                         startActivity(a);
